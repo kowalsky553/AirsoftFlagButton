55a40f11de41af692af91a7dba746ee23e1b0966
kowalsky553 <39545879+kowalsky553@users.noreply.github.com>
2023-03-05 14:33:54 +0400

    Initial

---
+++ BfButton/BfButton/Models/TeamColor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BfButton.Models
+{
+    enum TeamColor
+    {
+        Yellow,
+        Blue,
+        Green,
+        Mercs,
+        Neutral
+    }
+}
